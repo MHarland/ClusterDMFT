0b3d0de67fb08110fc564e35d7b3c66c5d839be8
MHarland <mharland@physnet.uni-hamburg.de>
2015-01-13 18:29:16 +0100

    new scripts

---
+++ bin/plot_pert_histo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env pytriqs
+from cdmft.cdmft import CDmft
+import sys
+from matplotlib import pyplot as plt, cm
+from numpy import loadtxt
+
+max_plot_order = 0
+for n, filename in enumerate(sys.argv[1:]):
+    data = loadtxt(filename)
+    max_order = 0
+    cum = 0
+    while cum < 1.0:
+        cum += data[max_order, 1]
+        max_order += 1
+    if max_order > max_plot_order: max_plot_order = max_order
+    plt.plot(data[:, 0], data[:, 1], label = filename[17:-4], color = cm.jet(n /float(len(sys.argv[1:]) - 1)))
+
+plt.gca().set_xlim(0, max_plot_order)
+plt.legend()
+plt.savefig('pert_histo.png', dpi = 300)
