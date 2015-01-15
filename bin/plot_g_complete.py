3dab0614ff6c147bd26e8c27aa8478fca8190a9b
MHarland <mharland@asterix01.physnet.uni-hamburg.de>
2015-01-15 17:24:56 +0100

    make plots nice

--- bin/plot_g_complete.py
+++ bin/plot_g_complete.py
@@ -12,6 +12,10 @@ for n, arch in enumerate(sys.argv[2:]):
     for s, b in g:
         for i in range(len(b.data[0,:,:])):
             for j in range(len(b.data[0,:,:])):
-                oplot(b[i, j], x_window = (0, max_freq))
-                plt.savefig(arch[0:-3] + '_G_' + s + '_' + str(i) + str(j) + '.png', dpi = 300)
+                oplot(b[i, j], x_window = (0, max_freq), RI = 'R', name = 'Re')
+                oplot(b[i, j], x_window = (0, max_freq), RI = 'I', name = 'Im')
+                if s == 'up': sout = '\\uparrow'
+                if s == 'down': sout = '\\downarrow'
+                plt.gca().set_ylabel('$G_{'+sout+'\\,'+str(i)+str(j)+'}^{(c)}(i\\omega_n)$')
+                plt.savefig(arch[0:-3]+'_G_'+s+'_'+str(i)+str(j)+'.png', dpi = 300)
                 plt.close()
