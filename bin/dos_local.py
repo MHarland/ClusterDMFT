d2ed987918bba5e4f7558a421fd53e7caae06c0c
MHarland <mharland@physnet.uni-hamburg.de>
2015-01-19 10:38:01 +0100

    cm.jet colors to dos plot

--- bin/dos_local.py
+++ bin/dos_local.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pytriqs
 from cdmft.periodization import Periodization
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, cm
 import sys
 
 n_start = int(sys.argv[1])
@@ -10,7 +10,7 @@ max_w = int(sys.argv[4])
 for arch in sys.argv[5:len(sys.argv)]:
     x = Periodization(archive = arch)
     for n in range(n_start, n_stop, n_step):
-        x.plot_dos_loc(pade_n_omega_n = n, pade_eta = 10**-10, dos_n_points = 1200, dos_window = (-max_w, max_w), name = str(n))
+        x.plot_dos_loc(pade_n_omega_n = n, pade_eta = 10**-10, dos_n_points = 1200, dos_window = (-max_w, max_w), name = str(n), color = cm.jet((n - n_start) * n_step/float(n_stop-n_start)))
     filename = 'dos_' + arch[0:-3] + '_' + str(n_start) + str(n_stop) + str(n_step) + '.png'
     plt.savefig(filename)
     plt.close()
