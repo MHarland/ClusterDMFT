a2715a6971ef5a70d71d751a9ec9bac907ff2396
MHarland <mharland@asterix01.physnet.uni-hamburg.de>
2015-01-30 17:16:17 +0100

    clean up

--- cellular_dmft.py
+++
@@ -1,8 +0,0 @@
-from .lattice.superlatticetools import _init_k_sum
-
-class cellular_dmft(object):
-    def __init__(self, lattice_vectors, clustersite_pos, hopping, n_kpts):
-        self.k_sum = _init_k_sum(lattice_vectors, clustersite_pos, hopping, n_kpts)
-
-    def g_local(self, sigma, mu):
-        return self.k_sum(mu = mu, Sigma = sigma)
