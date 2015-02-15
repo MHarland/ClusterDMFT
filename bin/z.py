b1bcc298ff78e1180034dca06ebcd14c161adb2a
MHarland <mharland@asterix01.physnet.uni-hamburg.de>
2015-02-15 18:48:20 +0100

    add up and down to Z evaluation routine

--- bin/z.py
+++ bin/z.py
@@ -8,5 +8,6 @@ for n, arch in enumerate(sys.argv[1:]):
     c = CDmft(archive = arch)
     print arch[:-3] + ':'
     sigma_iw = c.load('Sigma_c_iw')
-    z = [get_quasiparticle_residue(sigma_iw, n) for n in range(2, 6)]
-    print 'Z(nr of matsubara frequencies of fit) = ', z
+    for s in ['up', 'down']:
+        z = [get_quasiparticle_residue(sigma_iw, n, s) for n in range(2, 6)]
+        print 'Z_'+s+'(nr of matsubara frequencies of fit) = ', z
