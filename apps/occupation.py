b09efc054bb829da16b1e1afcc0243d8e4b5d934
MHarland <mharland@li-lin27.PHYSnet.uni-hamburg.de>
2015-06-11 14:10:29 +0200

    occupation numbers to Observables

---
+++ apps/occupation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pytriqs
+from cdmft.cdmft import CDmft
+from cdmft.evaluation.observables import Observables
+import sys
+
+for arch in sys.argv[1:]:
+    results = CDmft(archive = arch)
+    g_iw = results.load('g_c_iw')
+    obs = Observables(g_iw)
+    print arch[:-3] + ':'
+    occs = obs.occupation()
+    for occ in occs:
+        print 'n_'+str(occ[0][0])+'_'+str(occ[0][1])+': '+str(occ[1])
+    print 'n_total:'+str(obs.total_occupation())
+    print
--- evaluation/quasiparticle.py
+++ evaluation/quasiparticle.py
@@ -1,14 +1,14 @@
 from numpy import array, polyval, polyfit
 
-def get_quasiparticle_residue(sigma, n_freq, spin = 'up', orb = (0, 0)):
+def quasiparticle_residue(sigma, n_freq, spin = 'up', orb = (0, 0)):
     mesh = array([w.imag for w in sigma.mesh])[0: n_freq]
     sigma_values = sigma[spin].data[0: n_freq, orb[0], orb[1]].real
     fit_coefficients = polyfit(mesh, sigma_values, len(mesh) - 1)
-    derivative_coeff = derivative(fit_coefficients)
+    derivative_coeff = __derivative(fit_coefficients)
     z = 1 / (1 - derivative_coeff[-1])
     return z
     
-def derivative(polynomial_coefficients):
+def __derivative(polynomial_coefficients):
     deg = len(polynomial_coefficients)
     der = array([(deg - n - 1) * polynomial_coefficients[n] for n in range(deg - 1)])
     return der
