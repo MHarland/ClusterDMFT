9e08b1e8e972e39ebda16b6bb5f9728ecfca4d83
MHarland <mharland@idefix01.physnet.uni-hamburg.de>
2015-08-07 17:49:19 +0200

    add new Oberservable-plots

---
+++ apps/dosAtFermiLevel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env pytriqs
+from cdmft.cdmft import CDmft
+from cdmft.evaluation.observables import Observables
+import sys
+
+for arch in sys.argv[1:]:
+    results = CDmft(archive = arch)
+    g_iw = results.load('g_c_iw')
+    obs = Observables(g_iw)
+    print arch[:-3] + ':'
+    doss = obs.dosAtFermiLevel()
+    for dos in doss:
+        print 'n_'+str(dos[0][0])+'_'+str(dos[0][1])+': '+str(dos[1])
+    print 'n_total: '+str(obs.totDosAtFermiLevel())
+    print
