cb90fec62f932aa7f0a54a0d92813fdfa55675d9
MHarland <mharland@li-lin27.PHYSnet.uni-hamburg.de>
2016-01-26 14:39:38 +0100

    update example

--- example/parameters_l_dim.py
+++ example/parameters_l_dim.py
@@ -1,4 +1,4 @@
-from ClusterDMFT.lattice.superlattices import l_dim as my_superlattice
+from ClusterDMFT.lattice.superlattices import DimerInChain as my_superlattice
 
 sl = my_superlattice()
 p = dict()
@@ -24,7 +24,7 @@ p['impose_paramagnetism'] = False
 #p['site_symmetries'] =[[(0,0),(1,1)],[(0,1),(1,0)]] # unnecessary due to transformation
 
 # impurity
-p['transformation'] = sl.get_symmetry_transformation() # transformation on sitespace only
+p['transformation'] = sl.get_transf_orbital() # transformation on sitespace only
 p['g_transf_struct'] = sl.get_g_transf_struct_orbital()
 p['fit_tail'] = True
 p['tail_start'] = 50
@@ -34,8 +34,8 @@ p["max_time"] = -1
 p["length_cycle"] = 20
 p["n_warmup_cycles"] = 5 * 10**3
 p["n_cycles"] = int(10**5 *.5)
-p['n_iw'] = 1025
-p['n_tau'] = 10001
+p['n_iw'] = 1000
+p['n_tau'] = 10000
 p['make_histograms'] = False
 p['measure_g_tau'] = False# use tail-fit options if g_l is not measured
 p['measure_g_l'] = True
