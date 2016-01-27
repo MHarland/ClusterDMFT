4188d30066e03d03d03c1167dba94e54dcbed9b5
MHarland <hhh264@jrl02.jureca>
2016-01-27 09:15:13 +0100

    all blocks for plot_f_loops

--- apps/plot_f_loops.py
+++ apps/plot_f_loops.py
@@ -4,22 +4,23 @@ from pytriqs.plot.mpl_interface import oplot
 from matplotlib import pyplot as plt
 import sys
 
-functions = ['G_c_iw']
-loops = range(0,5,1)
-index = 'up'
-indices = [(0, 0)]
+functions = ['g_transf_iw']
+loops = range(0,3,1)
+blocks = ['G','M','X','Y']
+indices = [(0, 0), (0,1)]
 plotrange = (0, 40)
 
 for arch in sys.argv[1:]:
     x = CDmft(archive = arch)
     for f in functions:
-        for index2 in indices:
-            for i, c in enumerate(['I', 'R']):
-                plt.subplot(2, 1, 1 + i)
-                if c == 'I': plt.gca().set_title('Imaginary Part')
-                if c == 'R': plt.gca().set_title('Real Part')
-                for n, l in enumerate(loops):
-                    oplot(x.load(f, l)[index][index2], RI = c, x_window = plotrange, name = 'it' + str(l), color = plt.cm.jet(n/float(len(loops) - 1)))
-            plt.tight_layout()
-            plt.savefig(arch[0:-3] + '_' + f + '_' + str(index) + '_' + str(index2[0])+str(index2[1])+'.png', dpi = 300)
-            plt.close()
+        for index in blocks:
+            for index2 in indices:
+                for i, c in enumerate(['I', 'R']):
+                    plt.subplot(2, 1, 1 + i)
+                    if c == 'I': plt.gca().set_title('Imaginary Part')
+                    if c == 'R': plt.gca().set_title('Real Part')
+                    for n, l in enumerate(loops):
+                        oplot(x.load(f, l)[index][index2], RI = c, x_window = plotrange, name = 'it' + str(l), color = plt.cm.jet(n/float(len(loops) - 1)))
+                plt.tight_layout()
+                plt.savefig(arch[0:-3] + '_' + f + '_' + str(index) + '_' + str(index2[0])+str(index2[1])+'.pdf', dpi = 300)
+                plt.close()
