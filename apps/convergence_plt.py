abe6c8c2b82050eec437420d170548e70b990b31
Malte Harland <malte@dhcp-172-21-48-30.wlan.uni-hamburg.de.local>
2016-06-07 12:05:49 +0200

    change convergence_plt.py

--- apps/convergence_plt.py
+++ apps/convergence_plt.py
@@ -18,38 +18,6 @@ for fname in sys.argv[1:]:
     ax4 = fig.add_axes([.12,.1+3*.87/4,.76,.87/4])
     axes = [ax1, ax2, ax3, ax4]
     x = range(first_loop_nr, cdmft.next_loop())
-
-    analysis = ConvergenceAnalysis(cdmft, "g_transf_iw", first_loop_nr)
-    analysis.calc_distances(arg_range)
-    y = analysis.integrated_distances
-    n_colors = len(y[0]) * y[0].values()[0].shape[0]**2
-    colors = [matplotlib.cm.jet(float(i)/max(1, n_colors - 1)) for i in range(n_colors)]
-    c = 0
-    yint = np.zeros([len(y)])
-    n_orb = 0
-    for blockname, block in y[0].items():
-        for i, j in itt.product(*[range(block.shape[0])]*2):
-            y2 = np.array([yy[blockname][i, j].real for yy in y])
-            yint += y2
-            n_orb += 1
-            ax1.plot(x[1:], y2, label = blockname+str(i)+str(j), color = colors[c], alpha = .5)
-            c += 1
-    ax1.plot(x[1:], yint/n_orb, label = blockname+str(i)+str(j), color = 'black')
-    ax1.set_xlabel("$\mathrm{loop}$")
-    ax1.set_ylabel("$\\sum_n |\\Delta \\Re\\tilde{G}(i\\omega_n)|$")
-
-    c = 0
-    yint = np.zeros([len(y)])
-    for blockname, block in y[0].items():
-        for i, j in itt.product(*[range(block.shape[0])]*2):
-            y2 = np.array([yy[blockname][i, j].imag for yy in y])
-            yint += y2
-            ax2.plot(x[1:], y2, label = blockname+str(i)+str(j), color = colors[c], alpha = .5)
-            c += 1
-    ax2.plot(x[1:], yint/n_orb, label = blockname+str(i)+str(j), color = 'black')
-    ax2.set_ylabel("$\\sum_n |\\Delta \\Im\\tilde{G}(i\\omega_n)|$")
-    ax2.set_xticklabels([])
-
     p = cdmft.load('parameters')
     n_iw = p['n_iw']
     g_struct = p['g_transf_struct']
@@ -59,8 +27,41 @@ for fname in sys.argv[1:]:
     #blockindices = p['blockstates']
     #n_colors = len(blocknames) * len(blockindices)**2
     n_colors = len(blocknames) * len(blockindices[0])**2
-    
     colors = [matplotlib.cm.jet(float(i)/max(1, n_colors - 1)) for i in range(n_colors)]
+    
+    c = 0
+    for blocknr, blockname in enumerate(blocknames):
+        for i, j in itt.product(*[blockindices[blocknr]]*2):
+        #for i, j in itt.product(*[blockindices]*2):
+            for freq in [0]:
+                y = []
+                for loopnr in range(first_loop_nr, cdmft.next_loop()):
+                    s = cdmft.load('sigma_transf_iw', loopnr)
+                    y.append(s[blockname].data[n_iw + freq, i, j])
+                    rey = np.array(y).real
+                    imy = np.array(y).imag
+                    rey -= rey[0]
+                    imy -= imy[0]
+                ax1.plot(x, rey, label = blockname+str(i)+str(j), color = colors[c])
+                ax2.plot(x, imy, label = blockname+str(i)+str(j), color = colors[c])
+            c += 1
+    y = []
+    y2 = []
+    ax1b = ax1.twinx()
+    for loopnr in range(first_loop_nr, cdmft.next_loop()):
+        dmu = cdmft.load('dmu', loopnr)
+        density = cdmft.load('density', loopnr)
+        y.append(dmu)
+        y2.append(density)
+    y = np.array(y) - y[0]
+    y2 = np.array(y2) - y2[0]
+    ax1b.plot(x, y, label = "$\\tilde{mu}$", color = 'black')
+    ax1b.plot(x, y2, label = "$N$", color = 'gray')
+    ax1b.set_ylabel("$\\tilde{\mu}$")
+    ax2.set_ylabel("$\Im\\tilde{\Sigma}(i\\omega_0)$")
+    ax1.set_ylabel("$\Re\\tilde{\Sigma}(i\\omega_0)$")
+    ax2.set_xticklabels([])
+    
     c = 0
     for blocknr, blockname in enumerate(blocknames):
         for i, j in itt.product(*[blockindices[blocknr]]*2):
@@ -70,18 +71,16 @@ for fname in sys.argv[1:]:
                 for loopnr in range(first_loop_nr, cdmft.next_loop()):
                     s = cdmft.load('g_transf_iw', loopnr)
                     y.append(s[blockname].data[n_iw + freq, i, j])
-                ax4.plot(x, np.array(y).real, label = blockname+str(i)+str(j), color = colors[c])
-                ax3.plot(x, np.array(y).imag, label = blockname+str(i)+str(j), color = colors[c])
+                    rey = np.array(y).real
+                    imy = np.array(y).imag
+                    rey -= rey[0]
+                    imy -= imy[0]
+                ax3.plot(x, rey, label = blockname+str(i)+str(j), color = colors[c])
+                ax4.plot(x, imy, label = blockname+str(i)+str(j), color = colors[c])
             c += 1
     y = []
-    ax4b = ax4.twinx()
-    for loopnr in range(first_loop_nr, cdmft.next_loop()):
-        dmu = cdmft.load('dmu', loopnr)
-        y.append(dmu)
-    ax4b.plot(x, y, label = "$\\tilde{mu}$", color = 'gray')
-    ax4b.set_ylabel("$\\tilde{\mu}$")
-    ax3.set_ylabel("$\Im\\tilde{\Sigma}(i\\omega_0)$")
-    ax4.set_ylabel("$\Re\\tilde{\Sigma}(i\\omega_0)$")
+    ax4.set_ylabel("$\Im\\tilde{G}(i\\omega_0)$")
+    ax3.set_ylabel("$\Re\\tilde{G}(i\\omega_0)$")
     ax3.set_xticklabels([])
     ax4.set_xticklabels([])
 
